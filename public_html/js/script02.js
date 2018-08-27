2b625efdf6421bcc931c3aba9c8ea776593e3894
ADSFP <42434137+ADSFP@users.noreply.github.com>
2018-08-27 13:05:46 +0200

    update01

--- public_html/js/script02.js
+++ public_html/js/script02.js
@@ -7,6 +7,11 @@
 
 let minEndTime = "http://example.org#minendtime";
 let maxEndTime = "http://example.org#maxendtime";
+let connection = "http://example.org#Connection";
+let signal_predicate = "http://example.org#signalGroup";
+let eventstate = "http://example.org/eventstate/";
+const jsonld = require('jsonld');
+const N3 = require('n3');
 
 if(typeof(EventSource) !== "undefined") {
     var source = new EventSource("https://lodi.ilabt.imec.be:3002/");
@@ -14,10 +19,52 @@ if(typeof(EventSource) !== "undefined") {
         document.getElementById("titel").innerHTML = "Getting server updates";
     };
     
-    source.onmessage = function(event) {
-        document.getElementById("myDiv").innerHTML += event.data + "<br>";
+    var requestTimes = function() {
+        let allTriples = source.triples;
+        let allQuads = source.quads;
+        for (t in allTriples) {
+            tr = allTriples[t];
+            if (tr.predicate.value === signal_predicate) {
+                connection.signal_group = tr.object.value;
+            }
+        }
+        
+        for (q in allQuads) {
+            qd = allQuads[q];
+            console.log(qd);
+            if (qd.predicate.value === eventstate) {
+                minEndTime.minEndTime = tr.object.value;
+            }
+        }
     };
+    
+    var retrieveQuads = function(_jsonld) {
+        return new Promise ((resolve, reject) => {
+            jsonld.toRDF(_jsonld, {format: 'application/n-quads'}, (err, nquads) => {
+                resolve(nquads);
+            });
+        });
+    };
+    
+    var toQuads = async function(_client, _path) {
+        const doc = await retrieveDocument(_client, _path);
+        if (doc) {
+            const stringOfQuads = await retrieveQuads(JSON.parse(doc));
+            const parser = new N3.Parser();
+            const allQuads = parser.parse(stringOfQuads);
+            return allQuads;
+        }
+        else {
+            return [];
+        }
+    }
+    
+    source.onmessage = function(event) {
+        requestTimes();
+        //document.getElementById("myDiv").innerHTML += "deze site is van arne!!" + "<br>";
+    };
+} 
 
-} else {
+else {
     document.getElementById("myDIV").innerHTML = "No server updates";
 }
